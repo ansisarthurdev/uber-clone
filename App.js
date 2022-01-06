634701385c904342822ada897bcae216124ebb75
ansisarthurdev <ansisarthur.dev@gmail.com>
2022-01-06 21:58:12 +0200

    added redux

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
+//redux
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
 export default function App() {
 
   return ( 
-    <SafeAreaProvider style={{flex: 1}}>
-      <SafeAreaView style={{flex: 1}}>
-      <Container>
-        <Text>Lets build uber</Text>
-      </Container>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider style={{flex: 1}}>
+        <SafeAreaView style={{flex: 1}}>
+        <Container>
+          <Text>Lets build uber</Text>
+        </Container>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Provider>
   )
 
 }
